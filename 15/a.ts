a1a397b4ae15f918b35272ca7a65f7564744cd3d
Vincent Guinaudeau <vincent.guinau@gmail.com>
2022-12-27 00:09:55 +0100

    Day 15

---
+++ 15/a.ts
@@ -0,0 +1,95 @@
+
+import { readFile } from 'node:fs/promises'
+
+interface Vec {
+	x: number,
+	y: number,
+}
+
+interface Sensor extends Vec {
+	beacon: Vec,
+}
+
+interface Range {
+	start: number,
+	stop: number,
+}
+
+function compareRange(a: Range, b: Range) {
+	if (a.start !== b.start) {
+		return a.start - b.start
+	}
+	return a.stop - b.stop
+}
+
+function simplify_ranges(ranges: Range []) {
+	ranges.sort(compareRange)
+
+	for (let i = 0; i < ranges.length; i++) {
+		const range = ranges[i]
+		let toMerge  = 1
+		let current_stop = range.stop
+
+		while ((ranges[i + toMerge]?.start ?? Infinity) <= current_stop + 1) {
+			const range = ranges[i + toMerge]
+			current_stop = Math.max(current_stop, range.stop)
+
+			toMerge++
+		}
+
+		if (toMerge > 1) {
+			const range = ranges[i]
+			ranges.splice( i, toMerge, { start: range.start, stop: current_stop })
+		}
+	}
+}
+
+const input = (await readFile('./input.txt')).toString()
+const lines = input.split('\n').filter(e => e.length)
+
+// Sensor at x=2300471, y=2016823: closest beacon is at x=2687171, y=2822745
+
+const beacons: Vec [] = []
+
+const sensors: Sensor [] = lines.map(line => {
+	const x = +(/Sensor at x=(-?\d+)/.exec(line)?.[1] as string)
+	const y = +(/Sensor at x=-?\d+, y=(-?\d+)/.exec(line)?.[1] as string)
+	const beaconX = +(/is at x=(-?\d+)/.exec(line)?.[1] as string)
+	const beaconY = +(/is at x=-?\d+, y=(-?\d+)/.exec(line)?.[1] as string)
+
+	let beacon = beacons.find(beacon => beacon.x === beaconX && beacon.y === beaconY)
+
+	if (!beacon) {
+		beacon = {
+			x: beaconX,
+			y: beaconY,
+		}
+		beacons.push(beacon)
+	}
+
+	return {
+		x,
+		y,
+		beacon,
+	}
+})
+
+const TARGET_ROW = 2000000
+const cleared_ranges: Range [] = []
+
+for (let sensor of sensors) {
+	const clear_radius = Math.abs(sensor.x - sensor.beacon.x) + Math.abs(sensor.y - sensor.beacon.y)
+
+	const row_reach = clear_radius - Math.abs(sensor.y - TARGET_ROW)
+
+	if (row_reach >= 0) {
+		cleared_ranges.push({
+			start: sensor.x - row_reach,
+			stop: sensor.x + row_reach,
+		})
+	}
+}
+
+simplify_ranges(cleared_ranges)
+
+console.log(cleared_ranges.reduce((acc, range) => acc + (range.stop - range.start) + 1, 0) - beacons.filter(beacon => beacon.y === TARGET_ROW).length)
